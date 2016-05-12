8cbb808acc0479e7c9ba10da9455668a8514e91a
Sachin Hamirwasia <github@asksac.com>
2016-05-12 14:45:32 -0500

    Updated to use new header-navigation label

--- templates/header.php
+++ templates/header.php
@@ -18,8 +18,8 @@
 
     <nav class="collapse navbar-collapse" role="navigation">
       <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav navbar-nav']);
+      if (has_nav_menu('header-navigation')) :
+        wp_nav_menu(['theme_location' => 'header-navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav navbar-nav']);
       endif;
       ?>
     </nav>
